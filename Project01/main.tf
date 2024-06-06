7902c44c0eaae02c8302cc2550aba57b44184d26
greatgoing <nehanaaharr@gmail.com>
2024-06-06 12:59:16 +0530

    Update main.tf

--- Project01/main.tf
+++ Project01/main.tf
@@ -5,13 +5,7 @@ terraform {
       version = "~>3.34.0"
     }
   }
-  # required_version = "~> 1.3.6"
-  backend "azurerm" {
-    resource_group_name  = "rg-naharneha96"
-    storage_account_name = "sastoredata01"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
+  backend "azurerm" {}
 }
 
 provider "azurerm" {
@@ -22,21 +16,6 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-naharneha96" # change to your rg name
+  name     = var.resource_group_name
   location = var.location
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.prefix}-vnet"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = var.vnetAdressSpace
-}
-
-resource "azurerm_subnet" "subnet" {
-  for_each             = var.subnets
-  name                 = each.value.name
-  address_prefixes     = [each.value.address]
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
 }
